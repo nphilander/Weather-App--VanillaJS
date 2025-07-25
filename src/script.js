34ee93b7f6f9ec5a4581f187bec7ffc3de84c9e4
nokhwezi <nokhwezi13@gmail.com>
2025-07-25 03:27:48 +0200

    created functions to handle searching the city with the api and getting the temp with the city value

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ob2cf76b441t303bab3624054158f86a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(e) {
   e.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -7,3 +22,5 @@ function handleSearchSubmit(e) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Johannesburg");
